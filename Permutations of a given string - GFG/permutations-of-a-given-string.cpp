bf4d3f8f40c477ed39558d845bb19f8bec76c267
Vansh2500 <90461087+Vansh2500@users.noreply.github.com>
2023-06-14 14:43:43 +0530

    Added solution - LeetHub

--- Permutations of a given string - GFG/permutations-of-a-given-string.cpp
+++ Permutations of a given string - GFG/permutations-of-a-given-string.cpp
@@ -6,37 +6,33 @@ using namespace std;
 class Solution
 {
 	public:
-	set<string  > st;
-	void f(int ind , string s ,vector<int >v , string S,int  n ){
-             if(ind>=n){
-                 st.insert(s);
-             return ;
-             }
-             
-             
-             for(int i =0;i<n;i++)
-             {
-                if(v[i]==1 )
-                continue ; 
-                v[i]=1 ;
-                
-                 f(ind+1 , s+S[i] , v,S,n);
-                 v[i]=0;
-             }
-             
-             
-             	    
+set<string >st;
+  void	f(int ind , string S , string str, vector<int >&vis){
+	    if(ind>=S.size())
+	    {
+	       st.insert(str);
+	        return ; 
+	    }
+	    
+	    for(int i=0; i <S.size();i++)
+	    {
+	       
+	        if(vis[i]==1)
+	        continue;
+	        vis[i]=1;
+	        f(ind+1, S, str+S[i],vis);
+	        vis[i]=0;
+	    }
 	}
 		vector<string>find_permutation(string S)
 		{
-		    int n=S.size();
-		    vector<string > ans ;
-		    vector<int >v(S.size(), 0);
-		    string s ="";
-		     f(0 ,s , v ,S,n);
-		     for(auto it :st)
-		     ans.push_back(it);
-		     return ans; 
+		   vector<string >v ;
+		   vector<int >vis(S.size(),0);
+		   string str="";
+		   f(0,S ,str,vis);
+		   for(auto it:st)
+		   v.push_back(it);
+		   return v ;
 		}
 };
 
